ca0bcc40e91d48feb9f621dbe02a460db5262eb1
karthipannerselvam <118588657+karthipannerselvam@users.noreply.github.com>
2024-11-20 10:26:10 +0530

    Created Route page

--- dynamic_route/src/router/index.js
+++ dynamic_route/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import RealTimeMonitoring from "../views/RealTimeMonitoring.vue";
 import BusManagement from "../views/buses.vue"
+import RouteManagement from "../views/routes.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name:"BusManagement",
       component:BusManagement
 
+    },
+    {
+      path:'/routes',
+      name:"RouteManagement",
+      component:RouteManagement
     },
     
   ],
