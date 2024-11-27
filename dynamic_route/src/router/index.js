d6862d4abd1e8acf59bfe3758437435a7c1e90ae
karthipannerselvam <118588657+karthipannerselvam@users.noreply.github.com>
2024-11-27 23:31:05 +0530

    created dashbord page

--- dynamic_route/src/router/index.js
+++ dynamic_route/src/router/index.js
@@ -2,13 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 import RealTimeMonitoring from "../views/RealTimeMonitoring.vue";
 import BusManagement from "../views/buses.vue"
 import RouteManagement from "../views/routes.vue"
+import Dashboard from "../views/dashboard.vue"
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path:'/',
+      name:'Dashboard',
+      component:Dashboard
+    },
+    {
+      path: '/real',
       name: 'RealTimeMonitoring',
       component: RealTimeMonitoring,
     },
---
+++ dynamic_route/src/views/dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>DashBoard</h1>
+    </div>
+</template>
+<script>
+
+</script>
+<style  scoped>
+h1{
+    color: black;
+}
+</style>
